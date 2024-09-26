1fbfee5bfe764c2ceb5a462c63d86430606c4565
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-26 12:12:55 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import sys
-
 from dataclasses import dataclass
 
 # import sqlparse - available if you need it!
@@ -15,6 +14,8 @@ if command == ".dbinfo":
         # Uncomment this to pass the first stage
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
+        number_of_tables = sum(line.count(b"CREATE TABLE") for line in database_file)
         print(f"database page size: {page_size}")
+        print(f"number of tables: {number_of_tables}")
 else:
     print(f"Invalid command: {command}")
